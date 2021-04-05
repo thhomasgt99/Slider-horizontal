bb61224256afa9ab7cf7479138f90683c48f92d3
Thomas Bernal <thhomasgt99@gmail.com>
2021-04-04 19:50:26 -0500

    arrego definitivo del salto del slider

--- slider.js
+++ slider.js
@@ -1,48 +1,46 @@
- const slider = document.querySelector("#slider")
- let sliderSection = document.querySelectorAll(".slider__section")
- let sliderSectionLast = sliderSection[sliderSection.length -1]
+const slider = document.querySelector("#slider")
+let sliderSection = document.querySelectorAll(".slider__section")
+let sliderSectionLast = sliderSection[sliderSection.length -1]
 
- const bntLeft = document.querySelector("#btn-left")
- const bntRight = document.querySelector("#btn-right")
+const bntLeft = document.querySelector("#btn-left")
+const bntRight = document.querySelector("#btn-right")
 
- slider.insertAdjacentElement('afterbegin', sliderSectionLast)
+slider.insertAdjacentElement('afterbegin', sliderSectionLast)
 
- function Next(){
-    let sliderSectionFirst = document.querySelectorAll(".slider__section")[0]
-    slider.style.marginLeft = "-200%"
-    slider.style.transition = "all 1s"
-     setTimeout(function(){
-        slider.style.transition = "none"
-        slider.insertAdjacentElement('beforeend',sliderSectionFirst)
-        slider.style.marginLeft ="-100%"
-    },1000)
+function Next(){
+   let sliderSectionFirst = document.querySelectorAll(".slider__section")[0]
+   slider.style.marginLeft = "-200%"
+   slider.style.transition = "all 1s"
+    setTimeout(function(){
+       slider.style.transition = "none"
+       slider.insertAdjacentElement('beforeend',sliderSectionFirst)
+       slider.style.marginLeft ="-100%"
+   },1000)
 }
 
 function Prev(){
-    let sliderSection = document.querySelectorAll(".slider__section")
-    let sliderSectionLast = sliderSection[sliderSection.length -1]
-    slider.style.marginLeft = "0"
-    slider.style.transition = "all 1s"
-   setTimeout(function(){
-        slider.style.transition = "none"
-        slider.insertAdjacentElement('afterbegin', sliderSectionLast)
-        slider.style.marginLeft ="-100%"
-    },1000)
+   let sliderSection = document.querySelectorAll(".slider__section")
+   let sliderSectionLast = sliderSection[sliderSection.length -1]
+   slider.style.marginLeft = "0"
+   slider.style.transition = "all 1s"
+  setTimeout(function(){
+       slider.style.transition = "none"
+       slider.insertAdjacentElement('afterbegin', sliderSectionLast)
+       slider.style.marginLeft ="-100%"
+   },1000)
 }
 
 bntRight.addEventListener('click', function(){
-    Next()
+   Next()
 })
 
 bntLeft.addEventListener('click', function(){
-    Prev()
+   Prev()
 })
 
 setInterval(() => {
-   Next() 
+  Next() 
 }, 5000);
-
-
 
 
 
